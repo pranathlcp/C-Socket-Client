2a5cad38c8ebafa06e1d31df999061e5a1e2d565
Pranath Pussella <pranathlcp123@gmail.com>
2019-07-12 14:52:24 +0530

    Client is functional

--- client.c
+++ client.c
@@ -31,27 +31,29 @@ void *reader(void *arg) {
         }
 
         int msg_size[5]={msg_size_buffer[0] - '0', msg_size_buffer[1] - '0', msg_size_buffer[2] - '0', msg_size_buffer[3] - '0', msg_size_buffer[4] - '0'};
-        char msg_size_str[5];
+        char msg_size_str[6];
         int i=0;
         int index = 0;
         for (i=0; i<5; i++) {
             index += snprintf(&msg_size_str[index], 6-index, "%d", msg_size[i]);
         }
+
         memset(msg_size_buffer, 0, 5);
+
         char* strtoumax_endptr;
         int msg_size_int = strtoumax(msg_size_str, &strtoumax_endptr, 10);
 
         char *buffer = NULL;
-        buffer = malloc( (msg_size_int + 2)*sizeof(char) + 1);
+        buffer = malloc( (msg_size_int + 27)*sizeof(char) + 1);
 
-        int receive_message_content_size = recv(connected_socket, buffer, (msg_size_int + 2)*sizeof(char), 0);
+        int receive_message_content_size = recv(connected_socket, buffer, (msg_size_int + 27)*sizeof(char), 0);
 
         if (receive_message_content_size <= 0) {
             perror("Error with message");
         }
 
         printf("%s\n", buffer);
-
+        memset(buffer, 0, (msg_size_int + 27)* sizeof(char));
         free(buffer);
     }
 }
@@ -87,7 +89,6 @@ int main() {
     // Create a socket
     int network_socket;
     network_socket = socket(AF_INET, SOCK_STREAM, 0);
-    printf("net sock: %d\n", network_socket);
     if (network_socket == -1) {
         perror("Error in creating the network socket");
         return 0;
@@ -120,14 +121,20 @@ int main() {
         char msg_size_str[6];
         char *full_msg;
         m = input_string(stdin, 10);
-        sprintf(msg_size_str, "%05d", (int) strlen(m) + 25);
-        full_msg = malloc(sizeof(char)*(strlen(m) + 25) + 1);
+        int strlen_msg = strlen(m) + 27;
+        sprintf(msg_size_str, "%05d", strlen_msg);
+        full_msg = malloc(sizeof(char)*(strlen(m) + 27) + 1);
         strcpy(full_msg, msg_size_str);
         strcat(full_msg, m);
-        printf("Full Message: %s\n", full_msg);
-        send(network_socket, full_msg, strlen(full_msg), 0);
         free(m);
-        free(full_msg);
+        memset(msg_size_str, 0, 6);
+        if(strlen_msg != 27) {
+            send(network_socket, full_msg, strlen(full_msg), 0);
+            free(full_msg);
+        }
+
+
+
         usleep(100000);
     }
 
